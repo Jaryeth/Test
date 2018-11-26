2daefe41206c2aba95afcc4d7b15a33d2db8d491
Jaryeth <jaryeth@gmail.com>
2018-11-26 09:50:44 -0500

    Got a scipt to write text documents.

---
+++ Powershell/Testingwritingcode.ps1
@@ -0,0 +1,5 @@
+$texttest = 'This is a test of my text writing'
+$texttest > 'newfilefortesting.txt'
+
+$texttest2 = 'success! now to see if I can send it to a doc folder'
+$texttest2 > 'success!.txt'
